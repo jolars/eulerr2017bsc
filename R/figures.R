a7df6ed55f54f290eb6e94d2ecb81a8050aba8e0
Johan Larsson <johanlarsson@outlook.com>
2017-09-10 00:19:38 +0200

    Update with several new plots, and expand on some sections.

--- R/figures.R
+++ R/figures.R
@@ -83,7 +83,7 @@ fig_polyarea <- function() {
       lattice::panel.polygon(seglines, col = "slategray2")
       lattice::panel.polygon(pp[, 1:2], col = "grey90")
       lattice::panel.points(mid, pch = 4, col = 1)
-      lattice::panel.points(pp[, 1:2, drop = FALSE], col = 1, pch = 16)
+      lattice::panel.points(pp[, 1:2, drop = FALSE], col = 1, pch = 19)
     }
   )
 }
@@ -130,7 +130,130 @@ fig_intersection <- function() {
       lattice::panel.xyplot(x, y, ..., col = c("transparent", "steelblue3"))
       lattice::panel.abline(a = -ll[3, 1]/ll[2, 1], b = -ll[1, 1]/ll[2, 1])
       lattice::panel.abline(a = -ll[3, 2]/ll[2, 2], b = -ll[1, 2]/ll[2, 2])
-      lattice::panel.points(t(pp[1:2, ]), col = 1)
+      lattice::panel.points(t(pp[1:2, ]), col = 1, pch = 19)
     })
+  gridExtra::grid.arrange(p1, p2, p3, ncol = 3)
+}
+
+#' Show how to label ellipses.
+#'
+#' @return
+#' @export
+fig_vogel <- function() {
+  h <- c(0.2, 0.6)
+  k <- c(0.4, 0.4)
+  a <- c(0.4, .5)
+  b <- c(0.3, 0.6)
+  phi <- c(2.5, -1)
+  n <- 250
+  seqn <- seq(1, n, 1)
+  theta <- seqn*pi*(3 - sqrt(5))
+  rad <- sqrt(seqn/n)
+  x <- rad*cos(theta)
+  y <- rad*sin(theta)
+  p <- rbind(x, y, 1)
+
+
+  # Scale, rotate and translate to match the ellipse
+  p0 <- RConics::translation(c(h[1], k[1])) %*%
+     RConics::rotation(-phi[1]) %*%  RConics::scaling(c(a[1], b[1])) %*% p
+
+  inside <- ((p0[1, ] - h[2])*cos(phi[2]) + (p0[2, ] - k[2])*sin(phi[2]))^2/a[2]^2 +
+    ((p0[1, ] - h[2])*sin(phi[2]) - (p0[2, ] - k[2])*cos(phi[2]))^2/b[2]^2 <= 1
+
+  # Set up limits
+  tx <- atan2(-b*tan(phi), a)
+  ty <- atan2(b*tan(pi/2L - phi), a)
+
+  xlim <- range(h + a*cos(tx)*cos(phi) - b*sin(tx)*sin(phi),
+                h + a*cos(tx + pi)*cos(phi) - b*sin(tx + pi)*sin(phi))
+  ylim <- range(k + b*sin(ty)*cos(phi) + a*cos(ty)*sin(phi),
+                k + b*sin(ty + pi)*cos(phi) + a*cos(ty + pi)*sin(phi))
+
+  p1 <- lattice::xyplot(
+    x ~ y, aspect = "iso", pch = 20, xlab = "", ylab = "",
+    col = 1,
+    xlim = grDevices::extendrange(xlim),
+    ylim = grDevices::extendrange(ylim),
+    par.settings = list(axis.line = list(col = "transparent")),
+    scales = list(draw = FALSE),
+    panel = function(x, y, ...) {
+      lattice::panel.points(t(p0[1:2, !inside]), pch = 19, col = "grey80", cex = 0.5)
+      lattice::panel.points(t(p0[1:2, inside]), pch = 19, col = "steelblue2", cex = 0.5)
+      lattice::panel.points(t(p0[1:2, !inside][, 14]), pch = 19, col = 1, cex = 0.5)
+      lattice::panel.polygon(RConics::ellipse(c(a[1], b[1]),
+                                              c(h[1], k[1]),
+                                              phi[1]))
+      lattice::panel.polygon(RConics::ellipse(c(a[2], b[2]),
+                                              c(h[2], k[2]),
+                                              phi[2]))
+  })
+
+  centers <- eulerr:::locate_centers(h, k, a, b, phi, 1:2)
+
+  p2 <- lattice::xyplot(
+    x ~ y, aspect = "iso", pch = 19, xlab = "", ylab = "",
+    col = 1,
+    xlim = grDevices::extendrange(xlim),
+    ylim = grDevices::extendrange(ylim),
+    par.settings = list(axis.line = list(col = "transparent")),
+    scales = list(draw = FALSE),
+    panel = function(x, y, ...) {
+      lattice::panel.arrows(p0[1, !inside][14], p0[2, !inside][14],
+                            centers[1, 1], centers[2, 1],
+                            length = 0.05, col = "grey70")
+      lattice::panel.points(t(p0[1:2, !inside][, 14]), pch = 19, col = 1)
+
+      lattice::panel.polygon(RConics::ellipse(c(a[1], b[1]),
+                                              c(h[1], k[1]),
+                                              phi[1]))
+      lattice::panel.polygon(RConics::ellipse(c(a[2], b[2]),
+                                              c(h[2], k[2]),
+                                              phi[2]))
+    })
+
+  p3 <- lattice::xyplot(
+    x ~ y, aspect = "iso", pch = 19, xlab = "", ylab = "",
+    col = 1,
+    xlim = grDevices::extendrange(xlim),
+    ylim = grDevices::extendrange(ylim),
+    par.settings = list(axis.line = list(col = "transparent")),
+    scales = list(draw = FALSE),
+    panel = function(x, y, ...) {
+      lattice::panel.text(t(centers[1:2, 1]), labels = "42")
+      lattice::panel.polygon(RConics::ellipse(c(a[1], b[1]),
+                                              c(h[1], k[1]),
+                                              phi[1]))
+      lattice::panel.polygon(RConics::ellipse(c(a[2], b[2]),
+                                              c(h[2], k[2]),
+                                              phi[2]))
+    })
+
+  gridExtra::grid.arrange(p1, p2, p3, ncol = 3)
+}
+
+
+#' Shocase the SKYLINE-BL aglorithm for packing rectangles (ellipses)
+#'
+#' @return A series of plots showcasing how the skyline algorithm works.
+#' @export
+fig_skyline <- function() {
+  lattice::xyplot(1~1)
+}
+
+#' Plots a comparison of the hard set in venneuler paper
+#'
+#' @return A side-by-side comparison.
+#' @export
+fig_venneulerHard <- function() {
+  set.seed(1)
+  set <- c(A = 4, B = 6, C = 3, D = 2, E = 7, F = 3,
+           "A&B" = 2, "A&F" = 2, "B&C" = 2, "B&D" = 1,
+           "B&F" = 2, "C&D" = 1, "D&E" = 1, "E&F" = 1,
+           "A&B&F" = 1, "B&C&D" = 1)
+  p1 <- plot(venneuler::venneuler(set), main = "stress = 0.006")
+  p2 <- plot(eulerr::euler(set), main = "stress = 0.004")
+  p3 <- plot(eulerr::euler(set, shape = "ellipse"), main = "stress = 0")
+
   gridExtra::grid.arrange(p1, p2, p3, ncol = 3)
 }
