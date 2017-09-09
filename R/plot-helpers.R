e174ac9e8dc8e24b8279b07a7a857242528b2057
Johan Larsson <johanlarsson@outlook.com>
2017-09-10 00:20:23 +0200

    Add a replacement plot function for venneuler.

---
+++ R/plot-helpers.R
@@ -0,0 +1,34 @@
+#' Hijacks the venneuler plot method
+#'
+#' Hijacks the venneuler plot method to plot using grid instead of
+#' base graphics, provide appropriate limits and label positions.
+#'
+#' @param x An object of class 'VennDiagram'
+#' @param col Color postions on the gradient
+#' @param col.fn Color function to produce colors with
+#' @param alpha Opacity
+#' @param main Titles
+#' @param edges Ignored
+#' @param border Border color
+#' @param col.txt ignored
+#' @param ... Passed on to [plot.euler]
+#'
+#' @return Plots a venneuler diagram using eulerr methods.
+#' @export
+plot.VennDiagram <- function(x,
+                             col,
+                             col.fn = function(col) hcl(col * 360, 130, 60),
+                             alpha = 0.3,
+                             main = NULL,
+                             edges = 200,
+                             border = NA,
+                             col.txt = 1,
+                             ...) {
+  obj <- structure(list(), class = "euler")
+  obj$coefficients <- cbind(x$centers, r = x$diameters/2)
+  obj$fitted.values <- rep(1, length.out = 2^nrow(x$centers) - 1)
+  obj$original.values <- rep(1, length.out = 2^nrow(x$centers) - 1)
+
+  plot(obj, fill = col.fn(x$colors), fill_alpha = alpha, font = 1,
+       border = border, main = main)
+}
