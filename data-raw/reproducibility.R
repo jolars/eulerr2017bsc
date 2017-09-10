4d6ba1063f827ace2174407445d91aef925ff339
Johan Larsson <johanlarsson@outlook.com>
2017-09-10 20:55:50 +0200

    Add a simulation study for consistency.

---
+++ data-raw/reproducibility.R
@@ -0,0 +1,49 @@
+library(eulerr)
+library(venneuler)
+# library(VennDiagram)
+
+set.seed(1)
+
+
+# Look at consistency for circles first
+
+out <- data.frame(venneuler = double(),
+                  eulerr = double(),
+                  sets = integer(),
+                  it = integer())
+
+# Place 2 to 10 sets
+for (i in 2:10) {
+  for (j in 1:100) {
+    r <- runif(i, 0.3, 0.6)
+    x <- runif(i, 0, 1)
+    y <- runif(i, 0, 1)
+
+    pars <- as.vector(matrix(c(x, y, r), nrow = 3, byrow = TRUE))
+
+    config <- as.vector(eulerr:::intersect_ellipses(pars, circles = TRUE))
+    ids <- eulerr:::bit_indexr(i)
+
+    for (k in 1:nrow(ids)) {
+      names(config)[k] <- paste0(LETTERS[1:i][ids[k, ]], collapse = "&")
+    }
+
+
+    s1 <- venneuler::venneuler(config)$stress
+    s2 <- eulerr::euler(config)$stress
+
+    out <- rbind(out, cbind(venneuler = s1, eulerr = s2, sets = i, it = j))
+  }
+}
+
+data_consistency <-
+  tidyr::gather(out, key = "software", "stress", -sets, -it) %>%
+  dplyr::mutate(software = as.factor(software), sets = as.fa)
+
+p <- lattice::xyplot(stress ~ it | sets + software,
+                     data = data_consistency, auto.key = list(lines = TRUE, points = FALSE),
+                     type = "h")
+
+latticeExtra::useOuterStrips(p)
+
+usethis::use_data(data_consistency, internal = TRUE)
